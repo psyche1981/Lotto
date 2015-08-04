05ad8f25549a6db887a4b2e59e621fb7ccdec2e4
psyche1981 <spjones1981@gmail.com>
2015-08-04 19:07:23 +0100

    more user input

--- Lotto Generator/main.cpp
+++ Lotto Generator/main.cpp
@@ -3,14 +3,29 @@
 #include <ctime>
 #include <map>
 static int NUM_BALLS = 0;
+static int NUM_BALLS_TO_DRAW = 0;
 
 int main(int argc, char** argv)
 {
-	std::cout << "How many balls do you want to draw?" << std::endl;
-	int numBallsToDraw = 0;
-	std::cin >> numBallsToDraw;
-	std::cout << "How many balls do you want to be in the hat?" << std::endl;
-	std::cin >> NUM_BALLS;
+	std::cout << "Please select a Lotto format (enter the number and press return)" << std::endl<<std::endl;
+	std::cout << "1. National Lottery. 49 Balls, 6 Drawn" << std::endl;
+	std::cout << "2. Custom" << std::endl;
+	int in = 0;
+	std::cin >> in;
+	if (in == 1)
+	{
+		NUM_BALLS_TO_DRAW = 6;
+		NUM_BALLS = 49;
+	}
+	else if (in == 2)
+	{
+		std::cout << "How many balls do you want to draw?" << std::endl;
+		std::cin >> NUM_BALLS_TO_DRAW;
+		std::cout << "How many balls do you want to be in the hat?" << std::endl;
+		std::cin >> NUM_BALLS;
+	}
+
+	
 
 	std::map<int, int> ballFreqs;
 	for (int i = 1; i < NUM_BALLS + 1; i++)
@@ -36,7 +51,7 @@ int main(int argc, char** argv)
 	counter = 0;
 
 	std::cout << "This Weeks Balls Are Going To Be: " << std::endl << std::endl;;
-	while (counter < numBallsToDraw)
+	while (counter < NUM_BALLS_TO_DRAW)
 	{
 		for (auto& it = ballFreqs.begin(); it != ballFreqs.end(); it++)
 		{
